d7fd7c8307f30b88de1391954ba148a968b765be
TuyNhung <tuyetnhungtran9375@gmail.com>
2025-11-30 10:58:10 +0700

    Add files via upload

--- font/font_patch.py
+++ font/font_patch.py
@@ -21,6 +21,7 @@ LATIN_ACCENTED = LATIN_BASIC + [(0x00C0, 0x00FF)]                         # Lati
 LATIN_EXT_A = LATIN_ACCENTED + [(0x0100, 0x017F)]                         # Latin Extended-A
 CYRILLIC = [(0x0400, 0x04FF)]                                             # Cyrillic characters
 DIGITS = [(0x0030, 0x003A)]                                               # Digits 0–9
+VIETNAMESE = [(0x0180, 0x0250)]
 
 FONT_RANGES = {
     "ko": HANGUL_SYLLABLES,
@@ -38,7 +39,7 @@ FONT_RANGES = {
     "pl": LATIN_EXT_A,
     "ru": CYRILLIC,
     "ua": CYRILLIC,
-    "vi": LATIN_ACCENTED + LATIN_EXT_A + [(0x0180, 0x0250)],
+    "vi": LATIN_ACCENTED + LATIN_EXT_A + VIETNAMESE,
 }
 
 def load_config():
